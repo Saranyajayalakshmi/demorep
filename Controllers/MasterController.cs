12dffdf653bea834e969b3a985a2c930edc0e37f
Saranyajayalakshmi <124907714+Saranyajayalakshmi@users.noreply.github.com>
2023-02-10 14:58:08 +0530

    Add files via upload

---
+++ Controllers/MasterController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestWebAPI.Models;
+
+namespace TestWebAPI.Controllers
+{
+    public class MasterController : ApiController
+    {
+        Entities3 db = new Entities3();
+
+        //Add Post Method
+        public string Post(MasterT masterTS)
+        {
+            db.MasterTS.Add(masterTS);
+            db.SaveChanges();
+            return "Master Added";
+        }
+        //Get All Records
+        public IEnumerable<MasterT>Get()
+        {
+            return db.MasterTS.ToList();
+        }
+        //Get Single Master
+        public MasterT Get(int id)
+        {
+            MasterT masterT = db.MasterTS.Find(id);
+            return masterT;
+        }
+        //Update the Records
+        public string Put(int id,MasterT masterT)
+        {
+            var masterTS = db.MasterTS.Find(id);
+            masterT.MName= masterT.MName;
+            masterT.ExpressId= masterT.ExpressId;
+
+            db.Entry(masterT).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return "Master Updated";
+        }
+        //Delete the Record
+        public string Delete(int id)
+        {
+            MasterT masterT= db.MasterTS.Find(id);
+            db.MasterTS.Remove(masterT);
+            db.SaveChanges();
+            return "Deleted";
+        }
+
+    }
+
+}
+
---
+++ Controllers/TranscationController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestWebAPI.Models;
+
+namespace TestWebAPI.Controllers
+{
+    public class TranscationController : ApiController
+    {
+        Entities4 db = new Entities4();
+
+        //Add Post Method
+        public string Post(Transc transc)
+        {
+            db.Transcs.Add(transc);
+            db.SaveChanges();
+            return "Transcation Added";
+        }
+        //Get All Records
+        public IEnumerable<Transc> Get()
+        {
+            return db.Transcs.ToList();
+        }
+        //Get Single Master
+        public Transc Get(int id)
+        {
+            Transc transc = db.Transcs.Find(id);
+            return transc;
+        }
+        //Update the Records
+        public string Put(int id, Transc transc)
+        {
+            var trans = db.Transcs.Find(id);
+            trans.Descrip= transc.Descrip;
+            trans.TEXpress= transc.TEXpress;
+
+            db.Entry(transc).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return "Transaction Updated";
+        }
+        //Delete the Record
+        public string Delete(int id)
+        {
+           Transc transc = db.Transcs.Find(id);
+            db.Transcs.Remove(transc);
+            db.SaveChanges();
+            return "Deleted";
+        }
+    }
+}
